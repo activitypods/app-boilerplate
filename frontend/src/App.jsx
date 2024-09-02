0e2cfeb173c64c40878a1328c562885a6d26c34d
srosset81 <srosset81@gmail.com>
2024-09-02 19:15:47 +0200

    Use new ActivityPods packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Admin, CustomRoutes, Resource, Layout, memoryStore } from 'react-admin';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { BackgroundChecks, PodLoginPage, RedirectPage } from '@activitypods/react';
 import authProvider from './config/authProvider';
 import dataProvider from './config/dataProvider';
 import i18nProvider from './config/i18nProvider';
+import ontologies from './config/ontologies.json';
 import * as resources from './resources';
-import PodLoginPage from './pages/PodLoginPage/PodLoginPage';
-import RedirectPage from './pages/RedirectPage';
-import BackgroundChecks from './BackgroundChecks';
 
-const customPodProviders = import.meta.env.VITE_POD_PROVIDER_BASE_URL && [
-  { 'apods:baseUrl': import.meta.env.VITE_POD_PROVIDER_BASE_URL, 'apods:area': 'Local' }
-];
+const LoginPage = props => (
+  <PodLoginPage
+    customPodProviders={
+      import.meta.env.VITE_POD_PROVIDER_BASE_URL && [
+        { 'apods:baseUrl': import.meta.env.VITE_POD_PROVIDER_BASE_URL, 'apods:area': 'Local' }
+      ]
+    }
+    {...props}
+  />
+);
 
-const LoginPage = props => <PodLoginPage customPodProviders={customPodProviders} {...props} />;
-
-const LayoutWithBackgroundChecks = (props) => <BackgroundChecks><Layout {...props} /></BackgroundChecks>;
+const LayoutWithBackgroundChecks = props => (
+  <BackgroundChecks clientId={import.meta.env.VITE_BACKEND_CLIENT_ID}>
+    <Layout {...props} />
+  </BackgroundChecks>
+);
 
 const App = () => (
   <BrowserRouter>
@@ -33,7 +41,7 @@ const App = () => (
         <Resource key={key} name={key} {...resource.config} />
       ))}
       <CustomRoutes noLayout>
-        <Route path="/r" element={<RedirectPage />} />
+        <Route path="/r" element={<RedirectPage ontologies={ontologies} />} />
       </CustomRoutes>
     </Admin>
   </BrowserRouter>
