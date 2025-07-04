729f14b26f827a978b74f6ef82aae508c26219e2
srosset81 <srosset81@gmail.com>
2025-07-04 19:00:46 +0200

    Upgrade to ActivityPods 2.3

--- frontend/src/resources/events/EventShow.jsx
+++ frontend/src/resources/events/EventShow.jsx
@@ -1,10 +1,13 @@
 import { Show, SimpleShowLayout, TextField, DateField, TopToolbar, EditButton } from 'react-admin';
+import { ReferenceField } from '@semapps/field-components';
+import { RemoteShareButton } from '@activitypods/react';
 import JoinButton from '../../common/buttons/JoinButton';
 
 const EventShowActions = () => (
   <TopToolbar>
     <EditButton />
     <JoinButton color="primary" />
+    <RemoteShareButton color="primary" clientId={import.meta.env.VITE_BACKEND_CLIENT_ID} />
   </TopToolbar>
 );
 
@@ -12,8 +15,11 @@ const EventShow = () => (
   <Show actions={<EventShowActions />}>
     <SimpleShowLayout>
       <TextField source="name" />
-      <TextField source="content" />
+      <ReferenceField source="dc:creator" reference="contacts">
+        <TextField source="foaf:nick" />
+      </ReferenceField>
       <DateField source="startTime" />
+      <TextField source="content" />
     </SimpleShowLayout>
   </Show>
 );
